1fc380b421353a21d60d5fcc334d8dcb662304b1
ray-singh <rayansh365@gmail.com>
2025-01-03 12:01:26 +0530

    Restructuring folder

--- setup.py
+++ setup.py
@@ -1,19 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='QuantKit',
-    version='0.1.0',
-    description='Custom Financial Data Analysis Toolkit',
-    author='Rayansh Singh',
+    name="QuantKit",
+    version="0.1.0",
+    author="Rayansh Singh",
+    author_email="rayansh365@gmail.com",
+    description="A Python library for financial data analysis and portfolio management",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ray-singh/QuantKit",
     packages=find_packages(),
     install_requires=[
-        'pandas',
-        'numpy',
-        'matplotlib',
-        'yfinance',
-        'plotly',
-        'scipy',
-        'datetime',
-        'requests'
+        "yfinance",
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "scipy",
+        "datetime",
+        "seaborn"
     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
 )
